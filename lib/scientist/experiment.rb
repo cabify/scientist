6a1ae9d232fac5d6f13aad81fa59d88418cc3421
Manuel Alb <manuel.albarran@cabify.com>
2018-04-24 16:28:06 +0200

    Check idempotence of experiment

--- lib/scientist/experiment.rb
+++ lib/scientist/experiment.rb
@@ -197,6 +197,13 @@ module Scientist::Experiment
     name = (name || "control").to_s
     block = behaviors[name]
 
+    current_behaviors =
+      if idempotent?
+        behaviors
+      else
+        { name => block }
+      end
+
     if block.nil?
       raise Scientist::BehaviorMissing.new(self, name)
     end
@@ -211,8 +218,8 @@ module Scientist::Experiment
 
     observations = []
 
-    behaviors.keys.shuffle.each do |key|
-      block = behaviors[key]
+    current_behaviors.keys.shuffle.each do |key|
+      block = current_behaviors[key]
       observations << Scientist::Observation.new(key, self, &block)
     end
 
@@ -289,5 +296,9 @@ module Scientist::Experiment
     else
       !!raise_on_mismatches
     end
+  end
+
+  def idempotent?
+    true
   end
 end
